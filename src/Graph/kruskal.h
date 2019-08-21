0c3a3177abb599d995fa57b710080112dbf288da
Vadym Berezdetskyi <wadym.0b@gmail.com>
2019-08-21 23:24:14 +0300

    kruskal

---
+++ src/Graph/kruskal.h
@@ -0,0 +1,8 @@
+#ifndef ALGORITHMS_BREADTH_FIRST
+#define ALGORITHMS_BREADTH_FIRST
+
+void kruskal (vector < pair<int, pair<int, int>>) {
+
+}
+
+#endif // !ALGORITHMS_BREADTH_FIRST
--- src/utils/getGraph.h
+++ src/utils/getGraph.h
@@ -3,29 +3,29 @@
 
 #include <ctime>
 #include <random>
+#include <vector>
+#include <utility>
 
 using namespace std;
 
-int** getGraph (int size) {
+vector<pair<int, pair<int, int>> getGraphEdgesVector(int size) {
     random_device rd;
     default_random_engine e(rd());
-    uniform_int_distribution<int> getItem(0, 1);
+    uniform_int_distribution<int> getItem(0, 20);
 
-    int** array = new int *[size];
-
-    for (int i = 0; i < size; i++) {
-        array[i] = new int[size];
-    }
+	vector < pair<int, pair<int, int>> edges(size);
 
     for (int i = 0; i < size; i++) {
-        array[i][i] = 0;
-
-        for (int j = i + 1; j < size; j++) {
-            array[i][j] = array[j][i] = getItem(e);
-        }
+		edges.push_back(pair<int, pair<int, int>());
     }
 
-    return array;
+    return edges;
+}
+
+void printEdgesList(vector< pair<int, pair<int, int>> list) {
+	cout << "Graph:" << endl;
+	for (pair<int, pair<int, int>& edge : list) {
+	}
 }
 
 #endif //ALGORITHMS_GETGRAPH_H
